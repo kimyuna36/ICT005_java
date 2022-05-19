258cda2237af2561ca6646b3376c831da9f68a7f
Yuna Kim <105847264+kimyuna36@users.noreply.github.com>
2022-05-19 16:59:32 +0900

    first class java project
    yeah!

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕하세요?");
+		System.out.println("저는 자바를 처음 공부합니다.");
+		System.out.println("저의 나이는 23살입니다.");
+	}
+
+}
