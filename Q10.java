f8ac2f56c37e5bac863e71657608d734e030c957
Yuna Kim <105847264+kimyuna36@users.noreply.github.com>
2022-07-20 01:06:46 +0900

    Add files via upload

--- Q10.java
+++ Q10.java
@@ -9,94 +9,44 @@ public class Q10 {
 	System.out.print("15명의 학생의 성적 입력: ");
 	int [] scores = new int [15];
 	String[]grades=new String[10];
-	for(int i=0;i<15;i++) {
-		scores[i]=in.nextInt();
+	
+    int [] grade_Cnt = new int [10]; // 각 등급별 카운트 변수
+	
+	
+	for(int i=0;i<15;i++)
+	{
+		scores[i]=in.nextInt();	
 	}
-	//15개의 숫자를 입력받는다.
-	for(int i=0; i<10;i++) {
+	
+	for(int i=0;i<10;i++)
+	{
 		grades[i]=(i*10)+"~"+((i+1)*10)+":";
+		
+		grade_Cnt[i]=0;  //초기값 0 설정
 	}
 	
-	for(int i=0; i<10;i++) 
-		for(int j=0; j<15;j++) 
-			if((i*10)<=scores[j]&&scores[j]<=(i+1)*10) 
-			grades[i]+="*";	
-			
-		
 	
-		
-	for(int i=0;i<10;i++) {
-		System.out.println(grades[i]);
+	for(int i=0; i<10;i++)
+	{ 
+		for(int j=0; j<15;j++)
+		{
+			if((i*10)<=scores[j]&&scores[j]<(i+1)*10)
+			{
+				grades[i]+="*";
+				grade_Cnt[i]++; //해당 구간의 성적이 있는 경우 카운트
+			}
+		}
+	}
+	
+	for(int i=0;i<10;i++)
+	{
+		if(grade_Cnt[i]>0)
+		{
+			//if(scores[i]!=0){
+			System.out.println(grades[i]);
+		}
+	}
+
+//34 2 27 37 24 40 28 60 68 81 70 41 91 89 21
 	}
 }
-}
-//	while(true) {
-//		int inputScores=in.nextInt();
-//		
-//		int a= inputScores/10;
-//		scores[a]++;
-//	}
-//	for(int s=0;s<10;s++) {
-//		
-//			System.out.printf("%d~%d: %d \n",s*10,(s+1)*10,scores[s]);
-//		}
-//	}
-//	}
-//
-//
-//
-//
-//		
-//	for(int i=0;i<10;i++) {
-//		System.out.printf("%d~%d: \n" ,i*10,(i+1)*10);
-//	for (int i=0; i<15; i++) {
-//		scores[i]= in.nextInt();
-//		switch(scores[i]/10) {
-//		case 0:
-//			stars[0]++;
-//			break;
-//		case 1:
-//			stars[1]++;
-//			break;
-//		case 2:
-//			stars[2]++;
-//			break;
-//		case 3:
-//			stars[3]++;
-//			break;
-//		case 4:
-//			stars[4]++;
-//			break;
-//		case 5:
-//			stars[5]++;
-//			break;
-//		case 6:
-//			stars[6]++;
-//			break;
-//		case 7:
-//			stars[7]++;
-//			break;
-//		case 8:
-//			stars[8]++;
-//			break;
-//		case 9:
-//			stars[9]++;
-//			break;
-//		
-//		}
-//		for (int a=0; a<10; a++) {
-//			
-//			if(stars[a]!=0) {
-//				int b = stars[a];
-//				System.out.print(i*10 + "~" + (i+1)*10 + ":");
-//				for(int k=0; k<10; k++) {
-//				
-//					System.out.print("*");
-//				}
-//				System.out.println();			}
-//		}
-//		
-//	}
-//	}
-
-
